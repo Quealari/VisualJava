15c0a28cc2e672dd86fe328d01705ba08125648d
ilya <ilia.lobow@gmail.com>
2023-01-21 16:32:13 +0500

    изменен проект под https://javarush.com/groups/posts/2560-vvedenie-v-java-fx

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -3,21 +3,37 @@ package com.example.demo;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+
+import java.io.InputStream;
+
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-    }
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Dogs application");
+        primaryStage.setWidth(500);
+        primaryStage.setHeight(400);
 
-    public static void main(String[] args) {
-        launch();
+        InputStream iconStream =
+                getClass().getResourceAsStream("/someImage.png");
+        Image image = new Image(iconStream);
+        primaryStage.getIcons().add(image);
+
+        Button button = new Button("WOF WOF ???'");
+
+        button.setOnAction(event -> {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "WOF WOF WOF!!!");
+            alert.showAndWait();
+        });
+        Scene primaryScene = new Scene(button);
+        primaryStage.setScene(primaryScene);
+
+        primaryStage.show();
+
     }
 }
